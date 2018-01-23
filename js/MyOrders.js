c35ae7c54c131c5825ca029ce600e2d4a86e5f28
yinyv <yinyvcf@qq.com>
2018-01-23 11:36:48 +0800

    del

--- js/MyOrders.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $('.Orders_bars>li').click(function(){
-        $(this).siblings().children('a').css('color','#ccc');
-        $(this).children('a').css('color','#000');
-    })
-})
